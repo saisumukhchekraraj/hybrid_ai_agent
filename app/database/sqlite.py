e6e6a514b70f8fd26c6ea0ecf9bd012a2bac502a
Sai Sumukh <saisumukhchekraraj@gmail.com>
2026-06-25 10:07:52 +0000

    updated sqlite code in database

--- app/database/sqlite.py
+++ app/database/sqlite.py
@@ -144,6 +144,116 @@ def get_available_doctors(specialty, day):
     conn.close()
 
     return [dict(doctor) for doctor in doctors]
+def patient_exists(patient_id):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        """
+        SELECT patient_id
+        FROM patient_records
+        WHERE patient_id = ?
+        LIMIT 1
+        """,
+        (patient_id,)
+    )
+
+    patient = cursor.fetchone()
+
+    conn.close()
+
+    if patient:
+        return True
+
+    return False
+def doctor_exists(doctor_id):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        """
+        SELECT
+            doctor_id,
+            is_available
+        FROM doctor_records
+        WHERE doctor_id = ?
+        LIMIT 1
+        """,
+        (doctor_id,)
+    )
+
+    doctor = cursor.fetchone()
+
+    conn.close()
+
+    return doctor
+def book_appointment(
+    patient_id,
+    doctor_id,
+    appointment_date,
+    appointment_time
+):
+   
+    if not patient_exists(patient_id):
+        return {
+            "success": False,
+            "message": "Patient not found."
+        }
+
+    doctor = doctor_exists(doctor_id)
+
+    if doctor is None:
+        return {
+            "success": False,
+            "message": "Doctor not found."
+        }
+
+    if not doctor["is_available"]:
+        return {
+            "success": False,
+            "message": "Doctor not available."
+        }
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        """
+        INSERT INTO appointment_records
+        (
+            patient_id,
+            doctor_id,
+            appointment_date,
+            appointment_time,
+            appointment_status
+        )
+        VALUES (?, ?, ?, ?, ?)
+        """,
+        (
+            patient_id,
+            doctor_id,
+            appointment_date,
+            appointment_time,
+            "Scheduled"
+        )
+    )
+    appointment_id = cursor.lastrowid
+    cursor.execute(
+        """
+        UPDATE doctor_records
+        SET is_available = 0
+        WHERE doctor_id = ?
+        """,
+        (doctor_id,)
+    )
+
+    conn.commit()
+    conn.close()
+
+    return {
+        "success": True,
+        "message": "Appointment booked successfully.",
+        "appointment_id": appointment_id
+    }
 
    
 
